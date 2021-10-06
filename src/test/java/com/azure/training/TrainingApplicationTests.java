5412361e3da3e4c3d70010b405047fcf31e29443
Smruti <smrutiranjnsahoo@gmail.com>
2021-10-06 09:07:54 +0530

    Add files via upload

---
+++ src/test/java/com/azure/training/TrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
